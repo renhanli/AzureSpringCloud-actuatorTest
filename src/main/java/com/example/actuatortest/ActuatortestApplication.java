e3c15bc01bb67c4f5371b2d4a8d896bb169dd633
renhanli <hanren@microsoft.com>
2022-06-30 15:54:38 +0800

    actuator init

--- src/main/java/com/example/actuatortest/ActuatortestApplication.java
+++ src/main/java/com/example/actuatortest/ActuatortestApplication.java
@@ -2,11 +2,9 @@ package com.example.actuatortest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class ActuatortestApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatortestApplication.class, args);
